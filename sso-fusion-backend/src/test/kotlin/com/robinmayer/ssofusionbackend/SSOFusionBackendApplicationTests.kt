d5a7f0dcaae5146a48dcd37b07525bd760a5a92d
Robin Mayer <robin.mayer@mail.de>
2024-01-23 22:39:21 +0100

    setup project

---
+++ sso-fusion-backend/src/test/kotlin/com/robinmayer/ssofusionbackend/SSOFusionBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.robinmayer.ssofusionbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SSOFusionBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
